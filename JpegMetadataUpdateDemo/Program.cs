de27771ee049800280b5676590525c1b79f5449d
Alexander Shkirya <shkirya@gmail.com>
2018-02-17 18:28:05 +0300

    Init with test classes

---
+++ JpegMetadataUpdateDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using JpegMetadataUpdateLib;
+
+namespace JpegMetadataUpdateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** DEMO STARTS *****");
+
+            using(Process proc = new Process("<filename>"))
+            {
+                Console.WriteLine("Start processing image {0}", proc.Filename);
+                Console.WriteLine("End processing image {0}", proc.Filename);
+                Console.WriteLine("Exit processing image {0}", proc.Filename);
+            }
+        }
+    }
+}
---
+++ JpegMetadataUpdateLib/Process.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JpegMetadataUpdateLib
+{
+    public class Process: IDisposable
+    {
+        private bool disposed = false;
+
+        public string Filename { get; set; }
+
+        public Process(string filename)
+        {
+            if (string.IsNullOrEmpty(filename))
+                throw new ArgumentNullException("filename", "Mandatory parameter is not specify");
+
+            Filename = filename;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    //context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
